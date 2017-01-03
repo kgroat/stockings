196a65152a5af4287e74b8dae0545809cec06c54
Kevin Groat <apps@kevingroat.io>
2017-01-03 15:09:42 -0500

    Renamed pages and added useful links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { HomePage } from '../pages/home/home';
+import { LinksPage } from '../pages/links/links';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    LinksPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,8 +16,8 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    HomePage,
+    LinksPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
---
+++ src/pages/links/links.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'links-page',
+  templateUrl: 'links.html'
+})
+export class LinksPage {
+  constructor(public navCtrl: NavController) {
+  }
+}
